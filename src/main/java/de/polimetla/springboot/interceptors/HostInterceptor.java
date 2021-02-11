8a4d4c0db1fda11eb8e59438fa7c5f740dc97c6c
Sriharsha Polimetla <sri-harsha.polimetla@airbus.com>
2021-02-11 20:36:06 +0100

    throwing to caller

--- src/main/java/de/polimetla/springboot/interceptors/HostInterceptor.java
+++ src/main/java/de/polimetla/springboot/interceptors/HostInterceptor.java
@@ -9,12 +9,10 @@ public class HostInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(System.getenv("API_HOST"));
-        System.out.println(request.getServerName());
         if(request.getServerName().contains(System.getenv("API_HOST"))){
             return true;
         }
-        response.getWriter().write("This is not an authenticated route. Please access the API through https://" + System.getenv("API_HOST"));
+        response.getWriter().write("Expected: " + System.getenv("API_HOST") + "Actual: " + request.getServerName());
         response.setStatus(400);
         return false;
     }
